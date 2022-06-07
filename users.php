27a37281e57696ffa3b03240aec16bc9d89d0c0e
User <snegovskojm@gmail.com>
2022-06-07 15:07:13 +0300

    refactored codestyle

--- users.php
+++ users.php
@@ -1,13 +1,17 @@
 <?php
 require 'connection.php';
 
-class User {
+class User
+{
 
-    public function store () {
-        $user =  Connection::connect("https://gorest.co.in/public/v2/users", 'get');
+    public function store()
+    {
+        $user = Connection::connect("https://gorest.co.in/public/v2/users", 'get');
         return json_decode($user, true);
     }
-    public function add ($name ,$email, $gender, $status) {
+
+    public function add($name, $email, $gender, $status)
+    {
         Connection::$array = ['name' => $name, 'email' => $email, 'gender' => $gender, 'status' => $status];
         return Connection::connect("https://gorest.co.in/public/v2/users", 'post');
     }
