8c3813bef58a0fab90153c29a4f3faaaa76b439b
User <snegovskojm@gmail.com>
2022-06-07 13:13:20 +0300

    create adding user script for html form

---
+++ add_user.php
@@ -0,0 +1,16 @@
+<?php
+require 'users.php';
+
+$user = new User();
+
+if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['gender']) && !empty($_POST['status'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $gender = $_POST['gender'];
+    $status = $_POST['status'];
+
+    $user->add($name, $email, $gender, $status);
+}
+
+$referer = $_SERVER['HTTP_REFERER'];
+header("Location: $referer");
