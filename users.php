0961e0d8d411f78d325d3906d589f48986c8912e
User <snegovskojm@gmail.com>
2022-06-07 13:36:53 +0300

    add 'json_decode' in store method of User class

--- users.php
+++ users.php
@@ -4,7 +4,8 @@ require 'connection.php';
 class User {
 
     public function store () {
-        return Connection::connect("https://gorest.co.in/public/v2/users", 'get');
+        $user =  Connection::connect("https://gorest.co.in/public/v2/users", 'get');
+        return json_decode($user, true);
     }
     public function add ($name ,$email, $gender, $status) {
         Connection::$array = ['name' => $name, 'email' => $email, 'gender' => $gender, 'status' => $status];
