0116c22981727e7d3ea39d9d3abd63e0c289372d
User <snegovskojm@gmail.com>
2022-06-07 15:07:13 +0300

    refactored codestyle

--- connection.php
+++ connection.php
@@ -1,9 +1,13 @@
 <?php
-class Connection {
+
+class Connection
+{
     public static array $array;
-    static function connect(string $url, string $method){
+
+    static function connect(string $url, string $method): string
+    {
         $ch = curl_init($url);
-        curl_setopt($ch,CURLOPT_HTTPHEADER, array(
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'Accept:application/json',
             'Content-Type:application/json',
             "Authorization: Bearer af842cebac7e3eb29852f5198eea4a644de6eca7ffb87a68c0b25430121ec123"));
@@ -12,8 +16,8 @@ class Connection {
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
                 break;
             case 'post':
-                curl_setopt($ch,CURLOPT_POST, 1);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(self::$array,JSON_UNESCAPED_UNICODE));
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(self::$array, JSON_UNESCAPED_UNICODE));
                 break;
             case 'put':
                 curl_setopt($ch, CURLOPT_PUT, 1);
--- index.php
+++ index.php
@@ -2,11 +2,14 @@
 require 'users.php';
 $users = new User();
 $all = $users->store();
-printf($all);
 ?>
+<html>
 <DOCTYPE html>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+            crossorigin="anonymous"></script>
     <title>Users</title>
     <body>
     <table class="table table-striped">
@@ -18,30 +21,60 @@ printf($all);
             <th>status</th>
         </tr>
 
-            <?php foreach ($users->store() as $user) {
-                echo "<tr>";
-                echo "<td>".$user['id']."</td>";
-                echo "<td>".$user['name']."</td>";
-                echo "<td>".$user['email']."</td>";
-                echo "<td>".$user['gender']."</td>";
-                echo "<td>".$user['status']."</td>";
-                echo "</tr>";
-            } ?>
-
+        <?php foreach ($users->store() as $user) :
+            echo "<tr>";
+            echo "<td>" . $user['id'] . "</td>";
+            echo "<td>" . $user['name'] . "</td>";
+            echo "<td>" . $user['email'] . "</td>";
+            echo "<td>" . $user['gender'] . "</td>";
+            echo "<td>" . $user['status'] . "</td>";
+            echo "</tr>";
+        endforeach;
+        ?>
     </table>
-    <form method="post" action="add_user.php">
-        <label>name
-            <input type="text" name="name">
-        </label>
-        <label>email
-            <input type="email" name="email">
-        </label>
-        <label>gender
-            <input type="text" name="gender">
-        </label>
-        <label>status
-            <input type="text" name="status">
-        </label>
-        <input type="submit" value="add">
-    </form>
+    <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel"
+         tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalToggleLabel">Fill Fields</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+
+                    <form method="post" action="add_user.php">
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Name</label>
+                            <input type="text" name="name" class="form-control" id="exampleInputEmail1"
+                                   aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Email</label>
+                            <input type="email" name="email" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-check-label" for="exampleCheck1">Gender</label>
+                            <select class="form-select" aria-label="Default select example" name="gender">
+                                <option selected>Select Gender</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-check-label" for="exampleCheck1">Status</label>
+                            <select class="form-select" aria-label="Default select example" name="status">
+                                <option selected>Select Status</option>
+                                <option value="active">Active</option>
+                                <option value="inactive">Inactive</option>
+                            </select>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Add</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <a class="btn btn-primary" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Add User</a>
     </body>
+</html>
