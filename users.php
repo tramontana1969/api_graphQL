b23a2bb349e1693f42a42aa2375acf4412939ee6
User <snegovskojm@gmail.com>
2022-06-07 13:12:02 +0300

    add attributes -n add method of User class

--- users.php
+++ users.php
@@ -6,7 +6,8 @@ class User {
     public function store () {
         return Connection::connect("https://gorest.co.in/public/v2/users", 'get');
     }
-    public function add () {
+    public function add ($name ,$email, $gender, $status) {
+        Connection::$array = ['name' => $name, 'email' => $email, 'gender' => $gender, 'status' => $status];
         return Connection::connect("https://gorest.co.in/public/v2/users", 'post');
     }
 }
