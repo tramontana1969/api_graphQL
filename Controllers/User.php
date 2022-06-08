846775bcde9dcf970d476aced4c35611b634049b
User <snegovskojm@gmail.com>
2022-06-08 18:48:56 +0300

    renamed '' property to ''

--- Controllers/User.php
+++ Controllers/User.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once 'Connection.php';
 
 class User
@@ -13,21 +16,21 @@ class User
 
     public function add($name, $email, $gender, $status)
     {
-        Connection::$array = ['name' => $name, 'email' => $email, 'gender' => $gender, 'status' => $status];
+        Connection::$user_data = ['name' => $name, 'email' => $email, 'gender' => $gender, 'status' => $status];
 
         return Connection::connect("https://gorest.co.in/public/v2/users", 'post');
     }
 
     public function update($id, $name, $email, $gender, $status)
     {
-        Connection::$array = ['name' => $name, 'email' => $email, 'gender' => $gender, 'status' => $status];
+        Connection::$user_data = ['name' => $name, 'email' => $email, 'gender' => $gender, 'status' => $status];
 
         return Connection::connect("https://gorest.co.in/public/v2/users/{$id}", 'put');
     }
 
     public function delete($id)
     {
-        Connection::$array = ['id' => $id];
+        Connection::$user_data = ['id' => $id];
 
         return Connection::connect("https://gorest.co.in/public/v2/users/{$id}", 'delete');
     }
