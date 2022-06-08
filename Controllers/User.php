e692106011a14e163e53021f9b3e5b04d88358f2
User <snegovskojm@gmail.com>
2022-06-08 18:36:31 +0300

    add empty string before 'return' statement

--- Controllers/User.php
+++ Controllers/User.php
@@ -7,24 +7,28 @@ class User
     public function store()
     {
         $user = Connection::connect("https://gorest.co.in/public/v2/users", 'get');
+
         return json_decode($user, true);
     }
 
     public function add($name, $email, $gender, $status)
     {
         Connection::$array = ['name' => $name, 'email' => $email, 'gender' => $gender, 'status' => $status];
+
         return Connection::connect("https://gorest.co.in/public/v2/users", 'post');
     }
 
     public function update($id, $name, $email, $gender, $status)
     {
         Connection::$array = ['name' => $name, 'email' => $email, 'gender' => $gender, 'status' => $status];
+
         return Connection::connect("https://gorest.co.in/public/v2/users/{$id}", 'put');
     }
 
     public function delete($id)
     {
         Connection::$array = ['id' => $id];
+
         return Connection::connect("https://gorest.co.in/public/v2/users/{$id}", 'delete');
     }
 }
