0f273758b8075212b86fa047df58454b37dfb944
User <snegovskojm@gmail.com>
2022-06-08 15:09:26 +0300

    delete error reporting

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 require 'Controllers/User.php';
 $users = new User();
 $all = $users->store();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 ?>
 <html>
 <DOCTYPE html>
